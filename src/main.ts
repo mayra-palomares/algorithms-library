374686286578cc0072e5c08c5f838ea3e954a3d0
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-02-27 18:25:56 +0000

    Data Structures: Add Tree implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 // Data structures
-export { default as Graph } from "./data_structures/graph";
-export { default as LinkedList } from "./data_structures/linked_list";
-export { default as Queue } from "./data_structures/queue";
-export { default as Stack } from "./data_structures/stack";
+export { default as BinarySearchTree } from './data_structures/binary_search_tree';
+export { BinaryTreeNode } from './data_structures/binary_tree';
+export { default as Graph } from './data_structures/graph';
+export { default as LinkedList } from './data_structures/linked_list';
+export { default as Queue } from './data_structures/queue';
+export { default as Stack } from './data_structures/stack';
 
 // Searching algorithms
 
